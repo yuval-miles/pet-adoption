e0c891c7ecaa44b9385672790e5ace3199708ff4
yuval miles <yuvalmiles@gmail.com>
2022-09-04 11:35:55 +0300

    fixed typescript error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,25 @@ import type {
   AppLayoutProps,
   AppProps,
 } from "next/app";
-import { NextComponentType } from "next";
-import { ReactNode, useEffect } from "react";
+import { NextComponentType, NextPage } from "next";
+import { ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
+type Page<P = {}> = NextPage<P> & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+type Props = AppProps & {
+  Component: Page;
+};
+
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppLayoutProps> = ({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps) => {
+}: Props) => {
   const getLayout = Component.getLayout || ((page: ReactNode) => page);
   return (
     <SessionProvider session={session}>
